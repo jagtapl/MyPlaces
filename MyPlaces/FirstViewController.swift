6ddff651a9df5bf5158f23fd6d8b38260d2b143c
LALIT JAGTAP <jagtap_lalit@yahoo.com>
2018-04-10 08:27:38 -0500

    Initial Commit

---
+++ MyPlaces/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyPlaces
+//
+//  Created by LALIT JAGTAP on 4/10/18.
+//  Copyright © 2018 LALIT JAGTAP. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
